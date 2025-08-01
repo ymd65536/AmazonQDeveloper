8e2bdd0cbf7299173bd93c5748a89ee1d96f3265
Kento.Yamada <ymd65536@outlook.com>
2025-08-01 05:49:51 +0000

    create_embedding関数を定義し、埋め込み生成のロジックを整理

--- s3_vector/main.py
+++ s3_vector/main.py
@@ -3,29 +3,25 @@ import boto3
 import json
 
 
-def main():
+def create_embedding(
+        input_text: str = "Please recommend books with a theme similar to the movie 'Inception'.",
+        model_id: str = "amazon.titan-embed-text-v2:0"
+    ):
+    """
+    Create an embedding using the Amazon Bedrock model.
+    Returns:
+        dict: A dictionary containing the embedding and input text token count.
+    """
+
     print(f"boto3 version:{boto3.__version__}")
     client = boto3.client('bedrock-runtime', region_name='us-west-2')
 
-    # Set the model ID, e.g., Titan Text Embeddings V2.
-    model_id = "amazon.titan-embed-text-v2:0"
-
-    # The text to convert to an embedding.
-    input_text = "Please recommend books with a theme similar to the movie 'Inception'."
-
-    # Create the request for the model.
     native_request = {"inputText": input_text}
-
-    # Convert the native request to JSON.
     request = json.dumps(native_request)
 
-    # Invoke the model with the request.
     response = client.invoke_model(modelId=model_id, body=request)
-
-    # Decode the model's native response body.
     model_response = json.loads(response["body"].read())
 
-    # Extract and print the generated embedding and the input text token count.
     return {
         "embedding": model_response["embedding"],
         "inputTextTokenCount": model_response["inputTextTokenCount"]
